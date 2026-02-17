ebe5bfa5af5848571871e37d8031adc179f99292
John Brecht <john.brecht@gmail.com>
2026-02-16 16:46:26 -0800

    feat: Add server status check on application initialization using HttpClient.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { ProjectComponent } from './components/project/project.component';
 import { TapestryStatsComponent } from './components/tapestry-stats/tapestry-stats.component';
+import { environment } from '../environments/environment';
 import { UserMenuComponent } from './components/user-menu.component';
 
 @Component({
@@ -13,4 +15,16 @@ import { UserMenuComponent } from './components/user-menu.component';
 })
 export class App {
   protected readonly title = signal('tapestry');
+
+  constructor(private http: HttpClient) {
+    this.checkServerStatus();
+  }
+
+  private checkServerStatus() {
+    this.http.get(`${environment.apiUrl}/server-test`, { responseType: 'text' })
+      .subscribe({
+        next: (response) => console.log('Server status:', response),
+        error: (error) => console.error('Server status check failed:', error)
+      });
+  }
 }
